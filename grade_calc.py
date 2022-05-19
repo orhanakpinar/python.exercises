bece6f5e80417c5b23ac9edc27f61c080c9a3e29
root7Z <63443864+root7Z@users.noreply.github.com>
2022-05-19 23:40:59 +0200

    Update grade_calc.py

--- grade_calc.py
+++ grade_calc.py
@@ -12,28 +12,22 @@ except:
 try:
     grade = float(inp) 
 except:
-    print("Please enter a valid number")
+    print("Input value is not a number\nPlease enter a valid number")
+    quit()
 
 
-
-while grade >= 0 and grade <= 1:
+if grade >= 0 and grade <= 1:
     if grade < 0.6 and grade >=0:
         print("Score is F")
-        break
     elif grade < 0.7:
         print("Score is D")
-        break
     elif grade < 0.8 :
         print("Score is C")
-        break
     elif  grade < 0.9  :
         print("Score is B")
-        break
     elif grade <= 1:
         print("Score is A")
-        break
-    else:
-        print("enter a valid number")
+
 else:
     print("Please enter a valid number")
 
