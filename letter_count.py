5719e079394eaf2d8cccdd1465d79f882ba9d569
root7Z <63443864+root7Z@users.noreply.github.com>
2022-05-21 19:38:31 +0200

    Add files via upload

---
+++ letter_count.py
@@ -0,0 +1,25 @@
+#Python for Everybody, Chapter6 Exercise 3
+
+def letter_count():  
+    while True:    
+        inp_sent = input("Please enter a sentence or word to count the letter occurences:")
+        inp_word = input("Please enter the letter for searching through your entry:")
+        let_count = 0
+        inp_sent = inp_sent.lower()
+        inp_word = inp_word.lower()
+        if len(inp_word) > 1:
+            print("Please enter a single letter at a time.")
+        elif len(inp_word) == 0:
+            print("Please enter a letter.")
+        else:
+            let_count = 0
+            for lt in inp_sent:
+                if lt == inp_word:
+                    let_count = let_count + 1
+            print(let_count)
+        
+letter_count()
+
+
+
+
